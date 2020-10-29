d5b53b65cfbbc7b7646ec60b7ce862847050c6bb
Luis Cabrera Benito <parzibyte@gmail.com>
2020-10-28 21:38:12 -0600

    Limpieza y comentarios de código

--- memoria.py
+++ memoria.py
@@ -4,26 +4,44 @@ import math
 import time
 import random
 
+"""
+Iniciamos todo lo de Pygame para poder usar sonido, pantalla, etcétera
+"""
 pygame.init()
 pygame.font.init()
 pygame.mixer.init()
-NOMBRE_IMAGEN_OCULTA = "ocultar.png"
-MEDIDA_CUADRADO = 150
-SEGUNDOS_MOSTRAR_PIEZA = 1
 
+"""
+Variables y configuraciones que vamos a usar a lo largo del programa
+"""
+
+altura_boton = 30  # El botón de abajo, para iniciar juego
+medida_cuadro = 150  # Medida de la imagen en pixeles
+# La parte trasera de cada tarjeta
+NOMBRE_IMAGEN_OCULTA = "ocultar.png"
 imagen_oculta = pygame.image.load(NOMBRE_IMAGEN_OCULTA)
-imagen_oculta = pygame.transform.scale(imagen_oculta, (MEDIDA_CUADRADO, MEDIDA_CUADRADO))
+imagen_oculta = pygame.transform.scale(imagen_oculta, (medida_cuadro, medida_cuadro))
+SEGUNDOS_MOSTRAR_PIEZA = 1  # Segundos para ocultar la pieza si no es la correcta
+"""
+Una clase que representa el cuadro. El mismo tiene una imagen y puede estar
+descubierto (cuando ya lo han descubierto anteriormente y no es la tarjeta buscada actualmente)
+o puede estar mostrado (cuando se voltea la imagen)
+También tiene una fuente o nombre de imagen que servirá para compararlo más tarde
+"""
 
 
 class Cuadro:
     def __init__(self, fuente_imagen):
         self.mostrar = True
-        self.descubierta = False
+        self.descubierto = False
         self.fuente_imagen = fuente_imagen
         imagen_real = pygame.image.load(fuente_imagen)
-        self.imagen_real = pygame.transform.scale(imagen_real, (MEDIDA_CUADRADO, MEDIDA_CUADRADO))
+        self.imagen_real = pygame.transform.scale(imagen_real, (medida_cuadro, medida_cuadro))
 
 
+"""
+Todo el juego; que al final es un arreglo de objetos
+"""
 cuadros = [
     [Cuadro("conejo.jpg"), Cuadro("conejo.jpg"), Cuadro("leon.jpg"), Cuadro("leon.jpg")],
     [Cuadro("oveja.jpg"), Cuadro("oveja.jpg"), Cuadro("perro.jpg"), Cuadro("perro.jpg")],
@@ -31,15 +49,58 @@ cuadros = [
     [Cuadro("cocodrilo.jpg"), Cuadro("cocodrilo.jpg"), Cuadro("huron.jpg"), Cuadro("huron.jpg")],
 ]
 
+# Colores
+color_blanco = (255, 255, 255)
+color_negro = (0, 0, 0)
+color_gris = (206, 206, 206)
 
+# Los sonidos
+sonido_fondo = pygame.mixer.Sound("fondo.wav")
+sonido_clic = pygame.mixer.Sound("clic.wav")
+sonido_exito = pygame.mixer.Sound("exito.wav")
+sonido_fracaso = pygame.mixer.Sound("fracaso.wav")
+
+# Calculamos el tamaño de la pantalla en base al tamaño de los cuadrados
+anchura_pantalla = len(cuadros[0]) * medida_cuadro
+altura_pantalla = (len(cuadros) * medida_cuadro) + altura_boton
+anchura_boton = anchura_pantalla
+
+# La fuente que estará sobre el botón
+tamanio_fuente = 20
+fuente = pygame.font.SysFont("Arial", tamanio_fuente)
+xFuente = int((anchura_boton / 2) - (tamanio_fuente / 2))
+yFuente = int(altura_pantalla - altura_boton)
+
+# El botón, que al final de es un rectángulo
+boton = pygame.Rect(0, altura_pantalla - altura_boton, anchura_boton, altura_pantalla)
+
+# Banderas
+ultimos_segundos = None  # Bandera para saber si se debe ocultar la tarjeta dentro de N segundos
+puede_jugar = True  # Bandera para saber si reaccionar a los eventos del usuario
+juego_iniciado = False  # Saber si el juego está iniciado; así sabemos si ocultar o mostrar piezas, además del botón
+# Banderas de las tarjetas cuando se busca una pareja. Las necesitamos como índices para el arreglo de cuadros
+# x1 con y1 sirven para la primer tarjeta
+x1 = None
+y1 = None
+# Y las siguientes para la segunda tarjeta
+x2 = None
+y2 = None
+
+"""
+Funciones útiles
+"""
+
+
+# Ocultar todos los cuadros
 def ocultar_todos_los_cuadros():
     for fila in cuadros:
         for cuadro in fila:
             cuadro.mostrar = False
-            cuadro.descubierta = False
+            cuadro.descubierto = False
 
 
 def aleatorizar_cuadros():
+    # Elegir X e Y aleatorios, intercambiar
     cantidad_filas = len(cuadros)
     cantidad_columnas = len(cuadros[0])
     for y in range(cantidad_filas):
@@ -57,10 +118,11 @@ def comprobar_si_gana():
         reiniciar_juego()
 
 
+# Regresa False si al menos un cuadro NO está descubierto. True en caso de que absolutamente todos estén descubiertos
 def gana():
     for fila in cuadros:
         for cuadro in fila:
-            if not cuadro.descubierta:
+            if not cuadro.descubierto:
                 return False
     return True
 
@@ -75,54 +137,35 @@ def iniciar_juego():
     global juego_iniciado
     # Aleatorizar 3 veces
     for i in range(3):
-        # TODO: descomentar cuando se dejen de hacer pruebas
-        # aleatorizar_cuadros()
-        pass
+        aleatorizar_cuadros()
     ocultar_todos_los_cuadros()
-
-    # pygame.mixer.Channel(0).play(pygame.mixer.Sound("click.mpeg"))
     juego_iniciado = True
 
 
-sonido_fondo = pygame.mixer.Sound("fondo.wav")
-sonido_clic = pygame.mixer.Sound("clic.wav")
-sonido_exito = pygame.mixer.Sound("exito.wav")
-sonido_fracaso = pygame.mixer.Sound("fracaso.wav")
-pygame.mixer.Sound.play(sonido_fondo, -1)
-ALTURA_BOTON = 30
-anchura_pantalla = len(cuadros[0]) * MEDIDA_CUADRADO
-altura_pantalla = (len(cuadros) * MEDIDA_CUADRADO) + ALTURA_BOTON
-
-ANCHURA_BOTON = anchura_pantalla
+"""
+Iniciamos la pantalla con las medidas previamente calculadas, colocamos título y
+reproducimos el sonido de fondo
+"""
 pantalla_juego = pygame.display.set_mode((anchura_pantalla, altura_pantalla))
 pygame.display.set_caption('Memoria')
-
-boton = pygame.Rect(0, altura_pantalla - ALTURA_BOTON, ANCHURA_BOTON, altura_pantalla)
-
-cuadro_actual = None
-deberia_ocultar = False
-ultimos_segundos = None
-puede_jugar = True
-juego_iniciado = False
-x1 = None
-y1 = None
-x2 = None
-y2 = None
-
-color_blanco = (255, 255, 255)
-color_negro = (0, 0, 0)
-color_gris = (206, 206, 206)
-tamanio_fuente = 20
-fuente = pygame.font.SysFont("Arial", tamanio_fuente)
-xFuente = int((ANCHURA_BOTON / 2) - (tamanio_fuente / 2))
-yFuente = int(altura_pantalla - ALTURA_BOTON)
+pygame.mixer.Sound.play(sonido_fondo, -1)  # El -1 indica un loop infinito
+# Ciclo infinito...
 while True:
+    # Escuchar eventos, pues estamos en un ciclo infinito que se repite varias veces por segundo
     for event in pygame.event.get():
+        # Si quitan el juego, salimos
         if event.type == pygame.QUIT:
             sys.exit()
+        # Si hicieron clic y el usuario puede jugar...
         elif event.type == pygame.MOUSEBUTTONDOWN and puede_jugar:
 
-            xx, yy = event.pos
+            """
+            xAbsoluto e yAbsoluto son las coordenadas de la pantalla en donde se hizo
+            clic. PyGame no ofrece detección de clic en imagen, por ejemplo. Así que
+            se deben hacer ciertos trucos
+            """
+            # Si el click fue sobre el botón y el juego no se ha iniciado, entonces iniciamos el juego
+            xAbsoluto, yAbsoluto = event.pos
             if boton.collidepoint(event.pos):
                 if not juego_iniciado:
                     iniciar_juego()
@@ -131,11 +174,22 @@ while True:
                 # Si no hay juego iniciado, ignoramos el clic
                 if not juego_iniciado:
                     continue
-                x = math.floor(xx / MEDIDA_CUADRADO)
-                y = math.floor(yy / MEDIDA_CUADRADO)
+                """
+                Ahora necesitamos a X e Y como índices del arreglo. Los índices no
+                son lo mismo que los pixeles, pero sabemos que las imágenes están en un arreglo
+                y por lo tanto podemos dividir las coordenadas entre la medida de cada cuadro, redondeando
+                hacia abajo, para obtener el índice.
+                Por ejemplo, si la medida del cuadro es 100, y el clic es en 140 entonces sabemos que le dieron
+                a la segunda imagen porque 140 / 100 es 1.4 y redondeado hacia abajo es 1 (la segunda posición del
+                arreglo) lo cual es correcto. Por poner otro ejemplo, si el clic fue en la X 50, al dividir da 0.5 y
+                resulta en el índice 0
+                """
+                x = math.floor(xAbsoluto / medida_cuadro)
+                y = math.floor(yAbsoluto / medida_cuadro)
                 # Primero lo primero. Si  ya está mostrada o descubierta, no hacemos nada
                 cuadro = cuadros[y][x]
-                if cuadro.mostrar or cuadro.descubierta:
+                if cuadro.mostrar or cuadro.descubierto:
+                    # continue ignora lo de abajo y deja que el ciclo siga
                     continue
                 # Si es la primera vez que tocan la imagen (es decir, no están buscando el par de otra, sino apenas
                 # están descubriendo la primera)
@@ -153,20 +207,26 @@ while True:
                     cuadro2 = cuadros[y2][x2]
                     # Si coinciden, entonces a ambas las ponemos en descubiertas:
                     if cuadro1.fuente_imagen == cuadro2.fuente_imagen:
-                        cuadros[y1][x1].descubierta = True
-                        cuadros[y2][x2].descubierta = True
+                        cuadros[y1][x1].descubierto = True
+                        cuadros[y2][x2].descubierto = True
                         x1 = None
                         x2 = None
                         y1 = None
                         y2 = None
                     else:
                         pygame.mixer.Sound.play(sonido_fracaso)
-                        # Si no, tenemos que ocultarlas en el plazo de 1 segundo. Así que establecemos la bandera
+                        # Si no coinciden, tenemos que ocultarlas en el plazo de 1 segundo. Así que establecemos
+                        # la bandera. Como esto es un ciclo infinito y asíncrono, podemos usar el tiempo para saber
+                        # cuándo fue el tiempo en el que se empezó a ocultar
                         ultimos_segundos = int(time.time())
+                        # Hasta que el tiempo se cumpla, el usuario no puede jugar
                         puede_jugar = False
                 comprobar_si_gana()
 
     ahora = int(time.time())
+    # Y aquí usamos la bandera del tiempo, de nuevo. Si los segundos actuales menos los segundos
+    # en los que se empezó el ocultamiento son mayores a los segundos en los que se muestra la pieza, entonces
+    # se ocultan las dos tarjetas y se reinician las banderas
     if ultimos_segundos is not None and ahora - ultimos_segundos >= SEGUNDOS_MOSTRAR_PIEZA:
         cuadros[y1][x1].mostrar = False
         cuadros[y2][x2].mostrar = False
@@ -175,27 +235,38 @@ while True:
         x2 = None
         y2 = None
         ultimos_segundos = None
+        # En este momento el usuario ya puede hacer clic de nuevo pues las imágenes ya estarán ocultas
         puede_jugar = True
 
+    # Hacer toda la pantalla blanca
     pantalla_juego.fill(color_blanco)
+    # Banderas para saber en dónde dibujar las imágenes, pues al final
+    # la pantalla de PyGame son solo un montón de pixeles
     x = 0
     y = 0
+    # Recorrer los cuadros
     for fila in cuadros:
         x = 0
         for cuadro in fila:
-            if cuadro.descubierta or cuadro.mostrar:
+            """
+            Si está descubierto o se debe mostrar, dibujamos la imagen real. Si no,
+            dibujamos la imagen oculta
+            """
+            if cuadro.descubierto or cuadro.mostrar:
                 pantalla_juego.blit(cuadro.imagen_real, (x, y))
             else:
                 pantalla_juego.blit(imagen_oculta, (x, y))
-            x += MEDIDA_CUADRADO
-        y += MEDIDA_CUADRADO
+            x += medida_cuadro
+        y += medida_cuadro
 
     # También dibujamos el botón
     if juego_iniciado:
+        # Si está iniciado, entonces botón blanco con fuente gris para que parezca deshabilitado
         pygame.draw.rect(pantalla_juego, color_blanco, boton)
         pantalla_juego.blit(fuente.render("Iniciar juego", True, color_gris), (xFuente, yFuente))
     else:
         pygame.draw.rect(pantalla_juego, color_blanco, boton)
         pantalla_juego.blit(fuente.render("Iniciar juego", True, color_negro), (xFuente, yFuente))
 
+    # Actualizamos la pantalla
     pygame.display.update()
